44de842487bf5a7c1751eeae27418a46fd0b0eb2
CurtoBrull <curto.brull.javier@gmail.com>
2022-12-28 13:19:58 +0100

    Strong Params

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,6 +8,13 @@ class CoursesController < ApplicationController
   end
 
   def create
-    @course = Course.create title:params[:course][:title], description:params[:course][:description], visible:params[:course][:visible]
+    # @course = Course.create title:params[:course][:title], description:params[:course][:description], visible:params[:course][:visible]
+    @course = Course.create course_params
+    redirect_to courses_path
+  end
+
+  private
+  def course_params
+    params.required(:course).permit(:title, :description, :visible, :published)
   end
 end
